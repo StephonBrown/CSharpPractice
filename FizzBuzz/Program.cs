dfe4c49505e5f4993e1a29301b0bc9835c149f50
Stephon Brown <stephonb14@gmail.com>
2018-11-10 00:39:44 -0500

    Completed FizzBuzz exercise

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int counter = 1;
+
+            while (counter <= 100)
+            {
+                if (counter % 5 == 0 && counter % 3 == 0)
+                {
+                    Console.WriteLine(counter + " FizzBuzz");
+                }
+                else if (counter % 5 == 0)
+                {
+                    Console.WriteLine(counter + " Buzz");
+                }
+                else if (counter % 3 == 0)
+                {
+                    Console.WriteLine(counter + " Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(counter);
+                }
+
+                counter += 1;
+            }
+        }
+    }
+}
